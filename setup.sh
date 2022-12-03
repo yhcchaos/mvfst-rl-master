4f7b74db9db3d1f997a7d4116536f4c063a3de51
yhcchaos <yhcchaos@gmail.com>
2022-12-03 09:54:41 +0000

    second commit

---
+++ setup.sh
@@ -0,0 +1,201 @@
+#!/bin/bash -eu
+
+# Copyright (c) Facebook, Inc. and its affiliates.
+# All rights reserved.
+#
+# This source code is licensed under the license found in the
+# LICENSE file in the root directory of this source tree.
+#
+
+set -eu
+
+## Usage: ./setup.sh [--inference] [--skip-mvfst-deps]
+
+# Note: Pantheon requires python 2.7 while torchbeast needs python3.7.
+# Make sure your default python in conda env in python2.7 with an explicit
+# python3 command pointing to python 3.7
+
+# ArgumentParser
+INFERENCE=false
+SKIP_MVFST_DEPS=false
+POSITIONAL=()
+while [[ $# -gt 0 ]]; do
+  key="$1"
+  case $key in
+    --inference )
+      # If --inference is specified, only get what we need to run inference
+      INFERENCE=true
+      shift;;
+    --skip-mvfst-deps )
+      # If --skip-mvfst-deps is specified, don't get mvfst's dependencies.
+      SKIP_MVFST_DEPS=true
+      shift;;
+    * )    # Unknown option
+      POSITIONAL+=("$1") # Save it in an array for later
+      shift;;
+  esac
+done
+set -- ${POSITIONAL[@]+"${POSITIONAL[@]}"} # Restore positional parameters
+
+BUILD_ARGS=""
+MVFST_ARGS=""
+if [ "$INFERENCE" = true ]; then
+  echo -e "Inference-only build"
+  BUILD_ARGS="--inference"
+else
+  echo -e "Installing for training"
+fi
+if [ "$SKIP_MVFST_DEPS" = true ]; then
+  echo -e "Skipping dependencies of mvfst"
+  MVFST_ARGS="-s"
+fi
+
+PREFIX=${CONDA_PREFIX:-"/usr/local"}
+
+BASE_DIR="$PWD"
+BUILD_DIR="$BASE_DIR"/_build
+DEPS_DIR="$BUILD_DIR"/deps
+mkdir -p "$DEPS_DIR"
+
+if [ -d "$BUILD_DIR/build" ]; then
+  echo -e "mvfst-rl already installed, skipping"
+  exit 0
+fi
+
+PANTHEON_DIR="$DEPS_DIR"/pantheon
+LIBTORCH_DIR="$DEPS_DIR"/libtorch
+PYTORCH_DIR="$DEPS_DIR"/pytorch
+THIRDPARTY_DIR="$BASE_DIR"/third-party
+TORCHBEAST_DIR="$THIRDPARTY_DIR"/torchbeast
+MVFST_DIR="$THIRDPARTY_DIR"/mvfst
+
+cd "$BASE_DIR"
+git submodule sync && git submodule update --init --recursive
+
+function setup_pantheon() {
+  if [ -d "$PANTHEON_DIR" ]; then
+    echo -e "$PANTHEON_DIR already exists, skipping."
+    return
+  fi
+
+  # We clone Pantheon into _build/deps instead of using git submodule
+  # to avoid circular dependency - pantheon/third_party/ has
+  # this project as a submodule. For now, we clone and symlink
+  # pantheon/third_party/mvfst-rl to $BASE_DIR.
+  echo -e "Cloning Pantheon into $PANTHEON_DIR"
+  # TODO: Update repo url
+  git clone https://github.com/zj10/pantheon.git "$PANTHEON_DIR"
+
+  echo -e "Installing Pantheon dependencies"
+  cd "$PANTHEON_DIR"
+  ./tools/fetch_submodules.sh
+
+  # Install pantheon deps. Copied from pantheon/tools/install_deps.sh
+  # and modified to explicitly use python2-pip and install location.
+  sudo apt-get -y install mahimahi ntp ntpdate texlive python-pip
+  sudo apt-get -y install debhelper autotools-dev dh-autoreconf iptables \
+                          pkg-config iproute2
+  python2 -m pip install matplotlib numpy tabulate pyyaml
+
+  # Copy mahimahi binaries to conda env (to be able to run in cluster)
+  # with setuid bit.
+  cp /usr/bin/mm-* "$PREFIX"/bin/
+  sudo chown root:root "$PREFIX"/bin/mm-*
+  sudo chmod 4755 "$PREFIX"/bin/mm-*
+
+  # Install pantheon tunnel in the conda env.
+  cd third_party/pantheon-tunnel && ./autogen.sh \
+  && ./configure --prefix="$PREFIX" \
+  && make -j && sudo make install
+
+  # Force-symlink pantheon/third_party/mvfst-rl to $BASE_DIR
+  # to avoid double-building
+  echo -e "Symlinking $PANTHEON_DIR/third_party/mvfst-rl to $BASE_DIR"
+  ln -sf "$BASE_DIR" $PANTHEON_DIR/third_party/mvfst-rl
+  echo -e "Done setting up Pantheon"
+}
+
+function setup_libtorch() {
+  if [ -d "$LIBTORCH_DIR" ]; then
+    echo -e "$LIBTORCH_DIR already exists, skipping."
+    return
+  fi
+
+  # Install CPU-only build of PyTorch libs so that C++ executables of
+  # mvfst-rl such as traffic_gen don't need to be unnecessarily linked
+  # with CUDA libs, especially during inference.
+  echo -e "Installing libtorch CPU-only build into $LIBTORCH_DIR"
+  cd "$DEPS_DIR"
+
+  wget --no-verbose https://download.pytorch.org/libtorch/cpu/libtorch-cxx11-abi-shared-with-deps-1.2.0.zip
+
+  # This creates and populates $LIBTORCH_DIR
+  unzip libtorch-cxx11-abi-shared-with-deps-1.2.0.zip
+  rm -f libtorch-cxx11-abi-shared-with-deps-1.2.0.zip
+  echo -e "Done installing libtorch"
+}
+
+function setup_grpc() {
+  # Manually install grpc. We need this for mvfst-rl in training mode.
+  # Note that this gets installed within the conda prefix which needs to be
+  # exported to cmake.
+  echo -e "Installing grpc"
+  conda install -y -c anaconda protobuf=3.12.3
+  cd "$BASE_DIR" && ./third-party/install_grpc.sh
+  echo -e "Done installing grpc"
+}
+
+function setup_pytorch() {
+  if [ -d "$PYTORCH_DIR" ]; then
+    echo -e "$PYTORCH_DIR already exists, skipping."
+    return
+  fi
+
+  # TorchBeast requires PyTorch with CUDA. This doesn't conflict the CPU-only
+  # libtorch installation as the install locations are different.
+  echo -e "Installing PyTorch with CUDA for TorchBeast"
+  conda install -y numpy ninja pyyaml mkl mkl-include setuptools cmake cffi typing
+  conda install -y -c pytorch magma-cuda100
+
+  echo -e "Cloning PyTorch into $PYTORCH_DIR"
+  git clone -b v1.2.0 --recursive https://github.com/pytorch/pytorch "$PYTORCH_DIR"
+  cd "$PYTORCH_DIR"
+
+  export CMAKE_PREFIX_PATH=${PREFIX}
+  python3 setup.py install
+  echo -e "Done installing PyTorch"
+}
+
+function setup_torchbeast() {
+  echo -e "Installing TorchBeast"
+  cd "$TORCHBEAST_DIR"
+  python3 -m pip install -r requirements.txt
+
+  # Install nest
+  cd nest/ && CXX=c++ python3 -m pip install . -vv && cd ..
+
+  # export LD_LIBRARY_PATH=${PREFIX}/lib:${LD_LIBRARY_PATH}
+  export LD_LIBRARY_PATH=${PREFIX}/lib
+  CXX=c++ python3 setup.py install
+  echo -e "Done installing TorchBeast"
+}
+
+function setup_mvfst() {
+  # Build and install mvfst
+  echo -e "Installing mvfst"
+  cd "$MVFST_DIR" && ./build_helper.sh "$MVFST_ARGS"
+  cd _build/build/ && make install
+  echo -e "Done installing mvfst"
+}
+
+if [ "$INFERENCE" = false ]; then
+    setup_pantheon
+    setup_grpc
+    setup_pytorch
+    setup_torchbeast
+fi
+setup_libtorch
+setup_mvfst
+
+echo -e "Building mvfst-rl"
+cd "$BASE_DIR" && ./build.sh $BUILD_ARGS
---
+++ train/train.py
@@ -0,0 +1,274 @@
+#!/usr/bin/env python3
+
+# Copyright (c) Facebook, Inc. and its affiliates.
+# All rights reserved.
+#
+# This source code is licensed under the license found in the
+# LICENSE file in the root directory of this source tree.
+#
+
+# Run as python3 -m train.train
+
+import argparse
+import copy
+import logging
+import torch
+import torch.multiprocessing as mp
+import os
+import getpass
+import shutil
+from distutils.dir_util import copy_tree
+import sys
+
+from train import polybeast, pantheon_env, common, utils
+from train.constants import THIRD_PARTY_ROOT
+
+sys.path.append(THIRD_PARTY_ROOT)
+
+from gala.gpu_gossip_buffer import GossipBuffer
+from gala.graph_manager import FullyConnectedGraph as Graph
+
+logging.basicConfig(level=logging.INFO)
+
+os.environ["OMP_NUM_THREADS"] = "1"
+
+
+def get_parser():
+    parser = argparse.ArgumentParser()
+    common.add_args(parser)
+
+    polybeast_parser = parser.add_argument_group("polybeast")
+    polybeast.add_args(polybeast_parser)
+
+    pantheon_parser = parser.add_argument_group("pantheon_env")
+    pantheon_env.add_args(pantheon_parser)
+
+    parser.add_argument("--base_logdir", type=str, default="logs")
+    parser.add_argument("--base_tmpdir", type=str, default="/dev/shm/pantheon-data-" + getpass.getuser())
+
+    return parser
+
+
+def init_logdirs(flags):
+    pantheon_tmpdir = os.path.join('/dev/shm/', 'pantheon-tmp-' + getpass.getuser())
+    if os.path.exists(pantheon_tmpdir):
+        shutil.rmtree(pantheon_tmpdir)
+    if os.path.exists(flags.base_tmpdir):
+        shutil.rmtree(flags.base_tmpdir)
+    flags.logdir = os.path.join(flags.base_tmpdir, flags.mode)
+    flags.savedir = os.path.join(os.path.join(flags.base_logdir, flags.mode), "torchbeast")
+
+    # Clean run for test mode
+    if flags.mode != "train" and os.path.exists(flags.logdir):
+        shutil.rmtree(flags.logdir)
+
+    if flags.mode == "trace":
+        os.makedirs(flags.base_logdir, exist_ok=True)
+    else:
+        os.makedirs(flags.logdir, exist_ok=True)
+        os.makedirs(flags.savedir, exist_ok=True)
+
+    flags.checkpoint = os.path.join(flags.base_logdir, "checkpoint.tar")
+    flags.traced_model = os.path.join(flags.base_logdir, "traced_model.pt")
+
+    if flags.mode != "train":
+        assert os.path.exists(
+            flags.checkpoint
+        ), "Checkpoint {} missing in {} mode".format(flags.checkpoint, flags.mode)
+
+def move_logs():
+    copy_tree(flags.base_tmpdir, flags.base_logdir)
+    shutil.rmtree(flags.base_tmpdir)
+
+def make_gossip_buffer(flags, num_agents, mng, device):
+    """
+    Shared gossip buffer for GALA mode.
+    """
+    if num_agents <= 1:
+        return None, None
+
+    # Make topology
+    topology = []
+    for rank in range(num_agents):
+        graph = Graph(rank, num_agents, peers_per_itr=flags.num_gala_peers)
+        topology.append(graph)
+
+    # Initialize parameter buffer
+    flags.observation_shape = (1, 1, flags.observation_length)
+    model = polybeast.Net(
+        observation_shape=flags.observation_shape,
+        hidden_size=flags.hidden_size,
+        num_actions=flags.num_actions,
+        use_lstm=flags.use_lstm,
+    )
+    model.to(device)
+
+    # Keep track of local iterations since learner's last sync
+    sync_list = mng.list([0 for _ in range(num_agents)])
+    # Used to ensure proc-safe access to agents' message-buffers
+    buffer_locks = mng.list([mng.Lock() for _ in range(num_agents)])
+    # Used to signal between processes that message was read
+    read_events = mng.list(
+        [mng.list([mng.Event() for _ in range(num_agents)]) for _ in range(num_agents)]
+    )
+    # Used to signal between processes that message was written
+    write_events = mng.list(
+        [mng.list([mng.Event() for _ in range(num_agents)]) for _ in range(num_agents)]
+    )
+
+    # Need to maintain a reference to all objects in main processes
+    _references = [topology, model, buffer_locks, read_events, write_events, sync_list]
+    gossip_buffer = GossipBuffer(
+        topology,
+        model,
+        buffer_locks,
+        read_events,
+        write_events,
+        sync_list,
+        sync_freq=flags.sync_freq,
+    )
+    return gossip_buffer, _references
+
+
+def run_remote(flags, train=True):
+    flags.mode = "train" if train else "test"
+    # flags.disable_cuda = not train
+    flags.cc_env_mode = "remote"
+
+    proc_manager = mp.Manager()
+    barrier = None
+    shared_gossip_buffer = None
+    cuda = not flags.disable_cuda and torch.cuda.is_available()
+
+    num_agents = 1
+    if train and flags.num_gala_agents > 1:
+        # In GALA mode. Start multiple replicas of the polybeast-pantheon setup.
+        num_agents = flags.num_gala_agents
+        logging.info("In GALA mode, will start {} agents".format(num_agents))
+        barrier = proc_manager.Barrier(num_agents)
+
+        # Shared-gossip-buffer on GPU-0
+        device = torch.device("cuda:0" if cuda else "cpu")
+        shared_gossip_buffer, _references = make_gossip_buffer(
+            flags, num_agents, proc_manager, device
+        )
+
+    base_logdir = flags.base_logdir
+    polybeast_proc = []
+    pantheon_proc = []
+    for rank in range(num_agents):
+        flags.base_logdir = (
+            os.path.join(base_logdir, "gala_{}".format(rank))
+            if num_agents > 1
+            else base_logdir
+        )
+        init_logdirs(flags)
+
+        # Unix domain socket path for RL server address, one per GALA agent.
+        address = "/tmp/rl_server_path_{}".format(rank)
+        try:
+            os.remove(address)
+        except OSError:
+            pass
+        flags.address = "unix:{}".format(address)
+        flags.server_address = flags.address
+
+        # Round-robin device assignment
+        device = "cuda:{}".format(rank % torch.cuda.device_count()) if cuda else "cpu"
+
+        logging.info(
+            "Starting agent {} on device {}. Mode={}, logdir={}".format(
+                rank, device, flags.mode, flags.logdir
+            )
+        )
+        polybeast_proc.append(
+            mp.Process(
+                target=polybeast.main,
+                args=(flags, rank, barrier, device, shared_gossip_buffer),
+                daemon=False,
+            )
+        )
+        pantheon_proc.append(
+            mp.Process(target=pantheon_env.main, args=(flags,), daemon=False)
+        )
+        polybeast_proc[rank].start()
+        pantheon_proc[rank].start()
+
+    if train:
+        # Training is driven by polybeast. Wait until it returns and then
+        # kill pantheon_env.
+        for rank in range(num_agents):
+            polybeast_proc[rank].join()
+            pantheon_proc[rank].kill()
+    else:
+        # Testing is driven by pantheon_env. Wait for it to join and then
+        # kill polybeast.
+        for rank in range(num_agents):
+            pantheon_proc[rank].join()
+            polybeast_proc[rank].kill()
+
+    move_logs()
+    logging.info("Done {}".format(flags.mode))
+
+
+def test_local(flags):
+    flags.mode = "test"
+    init_logdirs(flags)
+
+    if not os.path.exists(flags.traced_model):
+        logging.info("Missing traced model, tracing first")
+        trace(copy.deepcopy(flags))
+
+    flags.cc_env_mode = "local"
+
+    logging.info("Starting local test, logdir={}".format(flags.logdir))
+    pantheon_proc = mp.Process(target=pantheon_env.main, args=(flags,), daemon=False)
+    pantheon_proc.start()
+    pantheon_proc.join()
+    move_logs()
+    logging.info("Done local test")
+
+
+def trace(flags):
+    flags.mode = "trace"
+    init_logdirs(flags)
+
+    logging.info("Tracing model from checkpoint {}".format(flags.checkpoint))
+    polybeast_proc = mp.Process(target=polybeast.main, args=(flags,), daemon=False)
+    polybeast_proc.start()
+    polybeast_proc.join()
+    logging.info("Done tracing to {}".format(flags.traced_model))
+
+
+def main(flags):
+    mode = flags.mode
+    logging.info("Mode={}".format(mode))
+
+    if mode == "train":
+        # Train, trace, and then test
+        run_remote(flags, train=True)
+        trace(flags)
+        run_remote(flags, train=False)
+    elif mode == "test":
+        # Only remote test
+        run_remote(flags, train=False)
+    elif mode == "test_local":
+        # Only local test
+        test_local(flags)
+    elif mode == "trace":
+        trace(flags)
+    else:
+        raise RuntimeError("Unknown mode {}".format(mode))
+
+    logging.info(
+        "All done! Checkpoint: {}, traced model: {}".format(
+            flags.checkpoint, flags.traced_model
+        )
+    )
+
+
+if __name__ == "__main__":
+    mp.set_start_method("spawn")
+    parser = get_parser()
+    flags = parser.parse_args()
+    main(flags)
